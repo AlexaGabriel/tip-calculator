13f080b55107c3434520eb31774df5efc5f3e96c
AlexaGabriel <alexandre181828@gmail.com>
2024-01-10 10:30:28 -0300

    cell

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const camposTexto = document.querySelectorAll('#texto');
 const botoes = document.querySelectorAll('.inputs');
+const error1 = document.querySelector('.button')
+const error2 = document.querySelector('.button2')
 const pessoas = document.querySelectorAll('.texto2');
 const result1 = document.querySelector('#thre');
 const result2 = document.querySelector('#six');
 const custom = document.querySelectorAll('.btn-tex');
 const resetBtn = document.querySelector('.reset')
+const err = document.querySelector('.ermen')
+const err1 = document.querySelector('.ermen1')
 
 let valorDoCampoTexto = 0;
 let valorDoBotao = 0;
@@ -32,8 +36,17 @@ resetBtn.addEventListener('click', resetValues);
 camposTexto.forEach(campo => {
     campo.addEventListener('input', () => {
         valorDoCampoTexto = parseFloat(campo.value);
+
+        if (isNaN(valorDoCampoTexto) || valorDoCampoTexto <= 0) {
+            error1.classList.add('erro');
+            err.innerHTML = ('can t be a zero') 
+          } else {
+            error1.classList.remove('erro');
+            err.innerHTML = ('') 
+          }
     });
 });
+
 
 botoes.forEach(botao => {
     botao.addEventListener('click', () => {
@@ -53,6 +66,13 @@ custom.forEach(bota => {
 pessoas.forEach(person => {
     person.addEventListener('input', () => {
         valorpessoas = parseFloat(person.value);
+        if (isNaN(valorpessoas) || valorpessoas <= 0) {
+            error2.classList.add('error');
+            err1.innerHTML = ('can t be a zero') 
+          } else {
+            error2.classList.remove('error');
+            err1.innerHTML = ('') 
+          }
         resultado = parc / valorpessoas;
         resultado2 = (parc + valorDoCampoTexto) / valorpessoas;
         console.log(`Resultado: ${resultado}`);
